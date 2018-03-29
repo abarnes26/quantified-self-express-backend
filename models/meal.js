926b2685fb0c4372fc8467c66d93ad71fad6661b
abarnes26 <abarnes26@yahoo.com>
2018-03-29 15:33:38 -0600

    altered sql query to account for missing food items

--- models/meal.js
+++ models/meal.js
@@ -6,7 +6,7 @@ const database = require('knex')(configuration)
 const Meal = {
 
   findAll: function() {
-    return database.raw("SELECT meals.*, array_agg(json_build_object('id',foods.id,'name',foods.name, 'calories', foods.calories)) AS foods FROM meals INNER JOIN meal_foods ON meals.id = meal_foods.meal_id INNER JOIN foods ON meal_foods.food_id = foods.id GROUP BY meals.id")
+    return database.raw("SELECT meals.*, array_agg(json_build_object('id',foods.id,'name',foods.name, 'calories', foods.calories)) AS foods FROM meals LEFT JOIN meal_foods ON meals.id = meal_foods.meal_id LEFT JOIN foods ON meal_foods.food_id = foods.id GROUP BY meals.id")
     .then(function(meals) {
       return meals.rows
     })
