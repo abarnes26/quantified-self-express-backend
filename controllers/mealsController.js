a36f33d56e180dd5c93e87585bdeb283e84bd5d2
BLaurenB <blbillington1@gmail.com>
2018-03-28 21:19:00 -0600

    Meals refactored to MVC style and all tests passing

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,8 +1,28 @@
-const Meal = require('../models/food')
+const Meal = require('../models/meal')
 
-const index = (req, res, next) => {
+const index = function(req, res, next) {
   Meal.findAll()
   .then(function(meals) {
     res.json(meals)
   })
 }
+
+const show = function(req, res, next) {
+  let id = req.params.id
+  Meal.find(id)
+  .then(function(meal) {
+    res.json(meal);
+  })
+}
+
+const update = function(req, res, next) {
+  let mealId = req.params.mealId
+  Meal.update(mealId)
+  .then(function(meal) {
+    res.status(201).json(meal.rows)
+  })
+}
+
+
+
+module.exports = {index, show, update}
--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -9,29 +9,8 @@ const mealsController = require('../../../controllers/mealsController')
 
 /* GET meals listing. */
 router.get('/', mealsController.index)
-  // database.raw("SELECT meals.*, array_agg(json_build_object('id',foods.id,'name',foods.name, 'calories', foods.calories)) AS foods FROM meals INNER JOIN meal_foods ON meals.id = meal_foods.meal_id INNER JOIN foods ON meal_foods.food_id = foods.id GROUP BY meals.id;")
-  // .then(function(meals) {
-  //   res.json(meals.rows);
-  // })
-
-
-router.get('/:id', function(req, res, next) {
-  var id = req.params.id
-  database.raw("SELECT meals.*, array_agg(json_build_object('id',foods.id,'name',foods.name, 'calories', foods.calories)) AS foods FROM meals INNER JOIN meal_foods ON meals.id = meal_foods.meal_id INNER JOIN foods ON meal_foods.food_id = foods.id WHERE meals.id = ? GROUP BY meals.id", id)
-  .then(function(meals) {
-    res.json(meals.rows[0]);
-    })
-  });
-
-
-router.get('/:mealId/foods/', function(req, res, next) {
-  var meal = req.params.mealId
-  database.raw('SELECT foods.* FROM foods INNER JOIN meal_foods ON meal_foods.food_id = foods.id WHERE meal_foods.meal_id = ?',
-   meal)
-  .then(function(meal) {
-    res.status(201).json(meal.rows)
-  })
-});
+router.get('/:id', mealsController.show)
+router.get('/:mealId/foods/', mealsController.update)
 
 
 
